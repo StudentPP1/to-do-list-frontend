268f35cc9547e7d0a0c92b7cfe93be16783fe10e
StudentPP1 <mishamakytonin@gmail.com>
2024-04-24 15:49:48 +0300

    add registration and login

--- src/API/UserService.jsx
+++ src/API/UserService.jsx
@@ -3,40 +3,49 @@ import axios from "axios";
 export default class UserService {
   
     static async register(user_email, user_password) {
-        const response = axios
-            .post(
-                "/auth/register", 
-            {
-                email: user_email,
-                password: user_password
-            },
-            
-            {
-                headers: {
-                    'Content-Type': "application/json"
-                }
-            });
-        return (await response).data
+        try {
+            const response = axios
+                .post(
+                    "/auth/register", 
+                {
+                    email: user_email,
+                    password: user_password
+                },
+                
+                {
+                    headers: {
+                        'Content-Type': "application/json"
+                    }
+                });
+            return (await response).data
+        } catch (error) {
+            return null
+        }
     }
 
     static async auth(user_email, user_password) {
-        let data = JSON.stringify({
-            "email": user_email,
-            "password": user_password
-          });
-          
-          let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: '/auth/auth',
-            headers: { 
-              'Content-Type': 'application/json'
-            },
-            data : data
-          };
-          
-          const response = axios.request(config)
-          return (await response).data
+        try {
+            let data = JSON.stringify({
+                "email": user_email,
+                "password": user_password
+              });
+              
+              let config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: '/auth/auth',
+                headers: { 
+                  'Content-Type': 'application/json'
+                },
+                data : data
+              };
+              
+            const response = axios.request(config)
+            return (await response).data
+        } catch (error) {
+            return null
+        }
+        
     }
 
     static async refreshToken(token) {
