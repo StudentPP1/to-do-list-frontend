42b6bcd3447ed55b0b38259caa57537bb6eb388a
StudentPP1 <mishamakytonin@gmail.com>
2024-07-24 19:54:44 +0300

    add div to oauth2handler

--- src/oauth2/OAuth2RedirectHandler.jsx
+++ src/oauth2/OAuth2RedirectHandler.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import {Navigate} from 'react-router-dom'
 import {AuthContext} from "../context";
 
@@ -12,17 +12,19 @@ const OAuth2RedirectHandler = () => {
     const token = getToken();
     console.log("token: ", token);
 
-    useEffect(() => {
-        return <Navigate to="/Today" replace />
-    }, [isAuth]);
-
     if (token) {
         localStorage.setItem('refreshToken', token);
         setIsAuth(true)
+        console.log('isAuth: ', isAuth);
     } else {
         return <Navigate to="/login" replace />
     }
 
+    return (
+        <div>
+
+        </div>
+    )
 
 }
 
