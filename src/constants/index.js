984f52d291ef53c02dcb110a6b4ce648a4e44704
StudentPP1 <mishamakytonin@gmail.com>
2024-07-24 17:54:08 +0300

    change constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://to-do-list-backend-2.onrender.com";
 
 export const ACCESS_TOKEN = "accessToken";
 
-export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
+export const OAUTH2_REDIRECT_URI = "https://to-do-list-frontend-wj7x.onrender.com/oauth2/redirect";
 
 export const GITHUB_AUTH_URL = API_BASE_URL + "/oauth2/authorize/github?redirect_uri=" + OAUTH2_REDIRECT_URI;
 
