72e57a7d49b9d1fdfd5cc957d84e0dd6e7845821
StudentPP1 <mishamakytonin@gmail.com>
2024-10-01 09:26:31 +0300

    update application properties

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,8 @@
-// http://localhost:8080
-export const API_BASE_URL = `${process.env.BACKEND_URL}`;
+export const API_BASE_URL = `${process.env.API_BASE_URL}`;
 
 export const ACCESS_TOKEN = "accessToken";
 
-// http://localhost:3000/oauth2/redirect
-export const OAUTH2_REDIRECT_URI = "`${process.env.FRONTEND_URL}`/oauth2/redirect";
+export const OAUTH2_REDIRECT_URI = `${process.env.FRONTEND_URL}/oauth2/redirect/oauth2/redirect`;
 
 export const GITHUB_AUTH_URL = API_BASE_URL + "/oauth2/authorize/github?redirect_uri=" + OAUTH2_REDIRECT_URI;
 
