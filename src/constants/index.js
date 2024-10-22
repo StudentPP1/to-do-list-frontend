69b2fb928ebd1f1e9fbf4f70981b046eec25d460
StudentPP1 <mishamakytonin@gmail.com>
2024-10-22 21:40:45 +0300

    fix bugs with jwt tokens in OAuth2

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ export const API_BASE_URL = `${process.env.REACT_APP_BACKEND_URL}`;
 
 export const ACCESS_TOKEN = "accessToken";
 
-export const OAUTH2_REDIRECT_URI = `${process.env.REACT_APP_FRONTEND_URL}/oauth2/redirect/oauth2/redirect`;
+export const OAUTH2_REDIRECT_URI = `${process.env.REACT_APP_FRONTEND_URL}/oauth2/redirect`;
 
 export const GITHUB_AUTH_URL = API_BASE_URL + "/oauth2/authorize/github?redirect_uri=" + OAUTH2_REDIRECT_URI;
 
--- src/oauth2/OAuth2RedirectHandler.jsx
+++ src/oauth2/OAuth2RedirectHandler.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import {Navigate} from 'react-router-dom'
 
 const OAuth2RedirectHandler = () => {
-    function getToken() {
-        return window.location.href.split('=')[1];
+    function getTokens() {
+        return window.location.href.split('=').slice(1);
     }
 
     const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-    const token = getToken()
-    if (token) {
+    const tokens = getTokens()
+    console.log(tokens)
+
+    if (tokens) {
         localStorage.removeItem('IsAuth')
          sleep(1000).then(() => {
+            localStorage.setItem("access_token", tokens[1]);
+            localStorage.setItem("refresh_token", tokens[0]);
             localStorage.setItem('IsAuth', "1");
-            localStorage.setItem('refresh_token', token);
             console.log(localStorage.getItem('IsAuth'));
         }).then(() =>
         {
